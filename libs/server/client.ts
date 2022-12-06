168f3cf4ef44e3e169697ceab950faf03c86032c
Seonwoo <wlstjsdn12@gmail.com>
2022-12-06 20:55:18 +0900

    .

--- libs/server/client.ts
+++ libs/server/client.ts
@@ -4,8 +4,6 @@ declare global {
   var client: PrismaClient | undefined;
 }
 
-const client = global.client || new PrismaClient();
+export const client = global.client || new PrismaClient();
 
-if (process.env.NODE_ENV === "development") global.client = client;
-
-export default client;
+if (process.env.NODE_ENV !== "production") global.client = client;
