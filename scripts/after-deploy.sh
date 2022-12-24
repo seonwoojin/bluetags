b42697567211b503aa2a229e562eb223d3878641
Seonwoo <wlstjsdn12@gmail.com>
2022-12-24 19:21:18 +0900

    Update after-deploy.sh

--- scripts/after-deploy.sh
+++ scripts/after-deploy.sh
@@ -1,8 +1,8 @@
-## after_install.sh ##
-
 #!/bin/bash
 
-cd /home/ubuntu/bluetags
-
-# zip 파일 풀기
-unzip -o -d /home/ubuntu/bluetags build.zip
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+export HOME="/home/ubuntu/"
+sudo PM2_HOME=/home/ubuntu/.pm2 pm2 list
+pm2 kill
